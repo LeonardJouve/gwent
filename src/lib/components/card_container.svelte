ba0ba3c0e4422b9f236d50a7ffbe0dddec417270
Léonard Jouve <leonard.jouve@gmail.com>
2025-07-10 12:44:53 +0200

    card container

---
+++ src/lib/components/card_container.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import type {CardData} from "$lib/types/card";
+    import Card from "./card.svelte";
+
+    type Props = {
+        cards: CardData[];
+    }
+    const {cards}: Props = $props();
+</script>
+
+<div class="hoverable hand">
+    {#each cards as card}
+        <Card card={card}/>
+    {/each}
+</div>
+
+<style>
+    .hand {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: end;
+        gap: 3px;
+        padding-bottom: 3px;
+    }
+</style>
--- src/lib/components/hand.svelte
+++ src/lib/components/hand.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
     import {store} from "$lib/store/hand.svelte";
-    import Card from "./card.svelte";
+    import CardContainer from "$lib/components/card_container.svelte";
 </script>
 
-<div class="hoverable hand">
-    {#each store.cards as card}
-        <Card card={card}/>
-    {/each}
-</div>
-
-<style>
-    .hand {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: end;
-        gap: 3px;
-        padding-bottom: 3px;
-    }
-</style>
+<CardContainer cards={store.cards}/>
