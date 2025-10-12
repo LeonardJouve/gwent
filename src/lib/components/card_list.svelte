eb8f8fb195f4b281849e5a758a64a38fea8b1463
Léonard Jouve <leonard.jouve@gmail.com>
2025-10-12 15:52:26 +0200

    stack deck cards

--- src/lib/components/card_list.svelte
+++ src/lib/components/card_list.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {largeClass} from "$lib/utils";
+    import {iconURL, largeClass} from "$lib/utils";
     import type {CardData} from "@shared/types/card";
 
     type Props = {
@@ -7,17 +7,36 @@
         onClick: (card: CardData) => void;
     };
     const {cards, onClick}: Props = $props();
+
+    type CardWithAmount = CardData & {amount: number};
+    const cardsWithAmount = $derived(cards.reduce<Map<CardData["filename"], CardWithAmount>>((acc, card) => {
+        const previousCard = acc.get(card.filename);
+        if (previousCard) {
+            previousCard.amount += 1;
+        } else {
+            acc.set(card.filename, {...card, amount: 1});
+        }
+
+        return acc;
+    }, new Map()).values());
 </script>
 
 <div class="card-list">
-    {#each cards as card}
+    {#each cardsWithAmount as card}
         <!-- svelte-ignore a11y_click_events_have_key_events -->
         <!-- svelte-ignore a11y_no_static_element_interactions -->
         <div
             class={[largeClass(card), "card"]}
-            style={`--count: "${card.maxPerDeck}"`}
             onclick={() => onClick(card)}
-        ></div>
+        >
+            <div class="amount">
+                <img
+                    src={iconURL("preview_count")}
+                    alt="amount"
+                />
+                <p>{card.amount}</p>
+            </div>
+        </div>
     {/each}
 </div>
 
@@ -35,20 +54,18 @@
         position: relative;
         --count: "0";
 
-        &::before {
-            content: var(--count);
-            background-image: url("assets/img/icons/preview_count.png");
-            position: absolute;
-            top: 82.5%;
-            left: 76%;
-            width: 20%;
-            height: 7%;
-            font-size: 1.2vw;
-            color: #5f4923;
-            padding-left: 1.7vw;
-            background-image: url("assets/img/icons/preview_count.png");
-            background-repeat: no-repeat;
-            background-size: contain;
-        }
+    }
+
+    .amount {
+        display: flex;
+        gap: 2px;
+        position: absolute;
+        top: 82.5%;
+        left: 55%;
+        width: 20%;
+        height: 7%;
+        font-size: 1.2vw;
+        color: #5f4923;
+        padding-left: 1.7vw;
     }
 </style>
